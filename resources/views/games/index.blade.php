533b75325b44c38926f88e747e140f2d058e7111
Robert Simpson <robert_simpson@hms.harvard.edu>
2016-05-10 17:21:33 -0400

    Add task index logic to track tasks by spin.

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -37,16 +37,41 @@
 
 
     <script>
-    
+    // add 7 degrees to offset arrow to middle of wheel slice
     var value = 7;
+    // set variable for spin counts
+    var spin_count = 1;
+    // set initial variable for tasks
+    var task_index = 0;
+    // set variable to save last task index
+    var last_task_index = 0;
+    // bind clickhandler to button using jQuerymobile methods
     $('#spinbutton').rotate({
         bind:
         {
             "click":function(){
                 // 360 degrees / 24 = 15 degree per wheel piece
                 var rot = Math.floor (Math.random() *24 ) * 15;
+                // always require 1 revolution
                 value += rot+360;
+                // rotate wheel using cumlative degrees
                 $('#image').rotate({ animateTo:value})
+
+                // calculate wheel task index 1 - 24
+                if (last_task_index != 0) {
+                    // remainder operator resets counter
+                    task_index = (last_task_index + (rot/15))%24;
+                } else {
+                    // first spin
+                    task_index = (rot/15);
+                }
+                //
+                $("p").append("Count: "+ spin_count +" Rot = "+ rot +" task_index: "+ task_index+ "<br>");
+
+                //save last task index, player spins where wheel landed
+                last_task_index = task_index;
+                // increment spin counter
+                spin_count++;
             }
         }
     });
