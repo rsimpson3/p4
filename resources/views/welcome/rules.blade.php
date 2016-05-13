ac13b2cc4dcd0ea5b851e6c740e4f7a48120b13f
Robert Simpson <robert_simpson@hms.harvard.edu>
2016-05-13 16:53:49 -0400

    Add rules and instructions.

--- resources/views/welcome/rules.blade.php
+++ resources/views/welcome/rules.blade.php
@@ -1,20 +1,37 @@
 @extends('layouts.master_welcome')
 
 @section('title')
-    Rules for the Wheel of Vernier Game
+    Rules and Instructions for the Wheel of Vernier Game
 @stop
 
 @section('content')
-    <h1>Rules</h1>
+    <h1>Rules &amp; Instructions</h1>
 
     <p>
-        Find a partner to play the game.
-        <ol>
-            <li> Rule One</li>
-            <li> Rule Two</li>
-            <li> Rule Three</li>
-        </ol>
+        Find a partner to play the game. Player 1 &amp; Player 2
     </p>
+
+    <ol>
+        <li>Each player must run the game on their own device.</li>
+        <li>Login or register as a new player to access the game.</li>
+        <li>New players must select their current skill level in using the Vernier Laquest device.</li>
+        <li>Click PLAY</li>
+        <li>Click Start Game to launch a new game.</li>
+        <li>Games require a minimun of 3 spins.</li>
+        <li>Click SPIN to randomly spin wheel.</li>
+        <li>Click BEGIN TASK </li>
+        <li>Demonstrate the task displayed using the Labquest device.</li>
+        <li>Player 2 cannot assist Player 1 in completing the task.</li>
+        <li>If player 1 completes task, then player 2 clicks DONE button on player 1's device.</li>
+        <li>Player 1 can choose to spin again or alternate turns between players.</li>
+        <li>After 3 spins by one player the game is over.</li>
+    </ol>
+
+    <p>
+        <b>Options:</b> Timed play is more challenging requiring players to complete tasks is a set amount of time. Easier tasks are 1 minute such <em>RUN</em>. Difficult tasks such as <em>Download to LoggerPro </em> have over 3 minutes.
+    </p>
+
+
 
         <!-- To get started <a href='/login'>log in</a> or <a href='/register'>register</a>. -->
 
